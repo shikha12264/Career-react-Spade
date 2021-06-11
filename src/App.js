b01020cc7b4e38435ef12d246d414eeb04a8df80
Shikha Singh <64529469+shikha12264@users.noreply.github.com>
2021-06-11 23:50:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,147 +1,267 @@
+import "tailwindcss/dist/base.css";
+import "styles/globalStyles.css";
 import React from "react";
-import { motion } from "framer-motion";
+
+/*
+ * This is the entry point component of this project. You can change the below exported default App component to any of
+ * the prebuilt landing page components by uncommenting their import and export lines respectively.
+ * See one of the landing page components to better understand how to import and render different components (Always
+ * make sure if you are building your own page, the root component should be the AnimationRevealPage component. You can
+ * disable the animation by using the disabled prop.
+ *
+ * The App component below is using React router to render the landing page that you see on the live demo website
+ * and the component previews.
+ *
+ */
+
+/* Use AnimationRevealPage as a wrapper component for your pages if you are building a custom one yourself */
+// import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+
+/*
+ * Hero section is the top most section on the page. It contains the header as well.
+ * So you dont need to import headers
+ * separately
+ */
+
+// import Hero from "components/hero/TwoColumnWithVideo.js";
+// import Hero from "components/hero/TwoColumnWithInput.js";
+// import Hero from "components/hero/TwoColumnWithFeaturesAndTestimonial.js";
+// import Hero from "components/hero/TwoColumnWithPrimaryBackground.js";
+// import Hero from "components/hero/FullWidthWithImage.js";
+// import Hero from "components/hero/BackgroundAsImage.js";
+// import Hero from "components/hero/BackgroundAsImageWithCenteredContent.js";
+
+// import Features from "components/features/ThreeColSimple.js";
+// import Features from "components/features/ThreeColWithSideImage.js";
+// import Features from "components/features/ThreeColWithSideImageWithPrimaryBackground.js";
+// import Features from "components/features/VerticalWithAlternateImageAndText.js";
+// import Features from "components/features/DashedBorderSixFeatures";
+// import MainFeature from "components/features/TwoColWithButton.js";
+// import MainFeature from "components/features/TwoColSingleFeatureWithStats.js";
+// import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
+// import MainFeature from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
+// import FeatureWithSteps from "components/features/TwoColWithSteps.js";
+// import FeatureStats from "components/features/ThreeColCenteredStatsPrimaryBackground.js";
+
+// import Pricing from "components/pricing/ThreePlans.js";
+// import Pricing from "components/pricing/ThreePlansWithHalfPrimaryBackground.js";
+// import Pricing from "components/pricing/TwoPlansWithDurationSwitcher.js";
+
+// import SliderCard from "components/cards/ThreeColSlider.js";
+// import TrendingCard from "components/cards/TwoTrendingPreviewCardsWithImage.js";
+// import Portfolio from "components/cards/PortfolioTwoCardsWithImage.js";
+// import TabGrid from "components/cards/TabCardGrid.js";
+
+// import Blog from "components/blogs/ThreeColSimpleWithImage.js";
+// import Blog from "components/blogs/ThreeColSimpleWithImageAndDashedBorder.js";
+// import Blog from "components/blogs/PopularAndRecentBlogPosts.js";
+// import Blog from "components/blogs/GridWithFeaturedPost.js";
+
+// import Testimonial from "components/testimonials/TwoColumnWithImage.js";
+// import Testimonial from "components/testimonials/TwoColumnWithImageAndProfilePictureReview.js";
+// import Testimonial from "components/testimonials/TwoColumnWithImageAndRating.js";
+// import Testimonial from "components/testimonials/ThreeColumnWithProfileImage.js";
+// import Testimonial from "components/testimonials/SimplePrimaryBackground.js";
+
+// import FAQ from "components/faqs/SimpleWithSideImage.js";
+// import FAQ from "components/faqs/SingleCol.js";
+// import FAQ from "components/faqs/TwoColumnPrimaryBackground.js";
+
+// import ContactUsForm from "components/forms/SimpleContactUs.js";
+// import ContactUsForm from "components/forms/TwoColContactUsWithIllustration.js";
+// import SubscribeNewsLetterForm from "components/forms/SimpleSubscribeNewsletter.js";
+//
+// import GetStarted from "components/cta/GetStarted.js";
+// import GetStarted from "components/cta/GetStartedLight.js";
+// import DownloadApp from "components/cta/DownloadApp.js";
+
+// import Footer from "components/footers/SimpleFiveColumn.js";
+// import Footer from "components/footers/FiveColumnWithInputForm.js";
+// import Footer from "components/footers/FiveColumnWithBackground.js";
+// import Footer from "components/footers/FiveColumnDark.js";
+// import Footer from "components/footers/MiniCenteredFooter.js";
+
+/* Ready Made Pages (from demos folder) */
+// import EventLandingPage from "demos/EventLandingPage.js";
+// import HotelTravelLandingPage from "demos/HotelTravelLandingPage.js";
+// import AgencyLandingPage from "demos/AgencyLandingPage.js";
+// import SaaSProductLandingPage from "demos/SaaSProductLandingPage.js";
+// import RestaurantLandingPage from "demos/RestaurantLandingPage.js";
+// import ServiceLandingPage from "demos/ServiceLandingPage.js";
+// import HostingCloudLandingPage from "demos/HostingCloudLandingPage.js";
+
+/* Inner Pages */
+// import LoginPage from "pages/Login.js";
+// import SignupPage from "pages/Signup.js";
+// import PricingPage from "pages/Pricing.js";
+// import AboutUsPage from "pages/AboutUs.js";
+// import ContactUsPage from "pages/ContactUs.js";
+// import BlogIndexPage from "pages/BlogIndex.js";
+// import TermsOfServicePage from "pages/TermsOfService.js";
+// import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
+
+//import ComponentRenderer from "ComponentRenderer.js";
+//import MainLandingPage from "MainLandingPage.js";
+//import ThankYouPage from "ThankYouPage.js";
+//import Career from "Career.js";
+//import Career2 from "Career2.js";
+//import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+
+
 import tw from "twin.macro";
-import styled from "styled-components";
 import { css } from "styled-components/macro"; //eslint-disable-line
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import Hero from "components/hero/TwoColumnWithVideo.js";
+import Features from "components/features/ThreeColSimple.js";
+import MainFeature from "components/features/TwoColWithButton.js";
+import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
+import TabGrid from "components/cards/TabCardGrid.js";
+import Testimonial from "components/testimonials/ThreeColumnWithProfileImage.js";
+import DownloadApp from "components/cta/DownloadApp.js";
+import Footer from "components/footers/FiveColumnWithInputForm.js";
 
-import useAnimatedNavToggler from "../../helpers/useAnimatedNavToggler.js";
+import chefIconImageSrc from "images/chef-icon.svg";
+import celebrationIconImageSrc from "images/celebration-icon.svg";
+import shopIconImageSrc from "images/shop-icon.svg";
 
-import logo from "../../images/logo.svg";
-import { ReactComponent as MenuIcon } from "feather-icons/dist/icons/menu.svg";
-import { ReactComponent as CloseIcon } from "feather-icons/dist/icons/x.svg";
-
-const Header = tw.header`
-  flex justify-between items-center
-  max-w-screen-xl mx-auto
-`;
-
-export const NavLinks = tw.div`inline-block`;
-
-/* hocus: stands for "on hover or focus"
- * hocus:bg-primary-700 will apply the bg-primary-700 class on hover or focus
- */
-export const NavLink = tw.a`
-  text-lg my-2 lg:text-sm lg:mx-6 lg:my-0
-  font-semibold tracking-wide transition duration-300
-  pb-1 border-b-2 border-transparent hover:border-primary-500 hocus:text-primary-500
-`;
-
-export const PrimaryLink = tw(NavLink)`
-  lg:mx-0
-  px-8 py-3 rounded bg-primary-500 text-gray-100
-  hocus:bg-primary-700 hocus:text-gray-200 focus:shadow-outline
-  border-b-0
-`;
-
-export const LogoLink = styled(NavLink)`
-  ${tw`flex items-center font-black border-b-0 text-2xl! ml-0!`};
-
-  img {
-    ${tw`w-10 mr-3`}
-  }
-`;
-
-export const MobileNavLinksContainer = tw.nav`flex flex-1 items-center justify-between`;
-export const NavToggle = tw.button`
-  lg:hidden z-20 focus:outline-none hocus:text-primary-500 transition duration-300
-`;
-export const MobileNavLinks = motion(styled.div`
-  ${tw`lg:hidden z-10 fixed top-0 inset-x-0 mx-4 my-6 p-8 border text-center rounded-lg text-gray-900 bg-white`}
-  ${NavLinks} {
-    ${tw`flex flex-col items-center`}
-  }
-`);
-
-export const DesktopNavLinks = tw.nav`
-  hidden lg:flex flex-1 justify-between items-center
-`;
-
-export default ({ roundedHeaderButton = false, logoLink, links, className, collapseBreakpointClass = "lg" }) => {
-  /*
-   * This header component accepts an optionals "links" prop that specifies the links to render in the navbar.
-   * This links props should be an array of "NavLinks" components which is exported from this file.
-   * Each "NavLinks" component can contain any amount of "NavLink" component, also exported from this file.
-   * This allows this Header to be multi column.
-   * So If you pass only a single item in the array with only one NavLinks component as root, you will get 2 column header.
-   * Left part will be LogoLink, and the right part will be the the NavLinks component you
-   * supplied.
-   * Similarly if you pass 2 items in the links array, then you will get 3 columns, the left will be "LogoLink", the center will be the first "NavLinks" component in the array and the right will be the second "NavLinks" component in the links array.
-   * You can also choose to directly modify the links here by not passing any links from the parent component and
-   * changing the defaultLinks variable below below.
-   * If you manipulate links here, all the styling on the links is already done for you. If you pass links yourself though, you are responsible for styling the links or use the helper styled components that are defined here (NavLink)
-   */
-  const defaultLinks = [
-    <NavLinks key={1}>
-      <NavLink href="/#">About Us</NavLink>
-      <NavLink href="/#">Careers</NavLink>
-      <NavLink href="/#">Testimonials</NavLink>
-      <NavLink href="/#">Department</NavLink>
-      <NavLink href="/#">Contact Us</NavLink>
-      <NavLink href="/#" tw="lg:ml-12!">
-        Login
-      </NavLink>
-      <PrimaryLink css={roundedHeaderButton && tw`rounded-full`}href="/#">Register Now</PrimaryLink>
-    </NavLinks>
-  ];
-
-  const { showNavLinks, animation, toggleNavbar } = useAnimatedNavToggler();
-  const collapseBreakpointCss = collapseBreakPointCssMap[collapseBreakpointClass];
-
-  const defaultLogoLink = (
-    <LogoLink href="/">
-      <img src={logo} alt="logo" />
-      SPADE
-    </LogoLink>
-  );
-
-  logoLink = logoLink || defaultLogoLink;
-  links = links || defaultLinks;
-
+export default function App() {
+  const Subheading = tw.span`tracking-wider text-sm font-medium`;
+  const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
+  const HighlightedTextInverse = tw.span`bg-gray-100 text-primary-500 px-4 transform -skew-x-12 inline-block`;
+  const Description = tw.span`inline-block mt-8`;
+  const imageCss = tw`rounded-4xl`;
   return (
-    <Header className={className || "header-light"}>
-      <DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
-        {logoLink}
-        {links}
-      </DesktopNavLinks>
+    <AnimationRevealPage>
+      <Hero
+        heading={<>Delicious & Affordable <HighlightedText>Meals Near You.</HighlightedText></>}
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        imageSrc="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80"
+        imageCss={imageCss}
+        imageDecoratorBlob={true}
+        primaryButtonText="Order Now"
+        watchVideoButtonText="Meet The Chefs"
+      />
+      <MainFeature
+        subheading={<Subheading>Established Since 2014</Subheading>}
+        heading={
+          <>
+            We have been serving for
+            <wbr /> <HighlightedText>over 5 years.</HighlightedText>
+          </>
+        }
+        description={
+          <Description>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+            <br />
+            <br />
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </Description>
+        }
+        buttonRounded={false}
+        textOnLeft={false}
+        primaryButtonText="Latest Offers"
+        imageSrc={
+          "https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80"
+        }
+        imageCss={imageCss}
+        imageDecoratorBlob={true}
+        imageDecoratorBlobCss={tw`left-1/2 -translate-x-1/2 md:w-32 md:h-32 opacity-25`}
+      />
+      {/* TabGrid Component also accepts a tabs prop to customize the tabs and its content directly. Please open the TabGrid component file to see the structure of the tabs props.*/}
+      <TabGrid
+        heading={
+          <>
+            Checkout our <HighlightedText>menu.</HighlightedText>
+          </>
+        }
+      />
+      <Features
+        heading={
+          <>
+            Amazing <HighlightedText>Services.</HighlightedText>
+          </>
+        }
+        cards={[
+          {
+            imageSrc: shopIconImageSrc,
+            title: "230+ Locations",
+            description: "Lorem ipsum donor amet siti ceali placeholder text",
+            url: "https://google.com"
+          },
+          {
+            imageSrc: chefIconImageSrc,
+            title: "Professional Chefs",
+            description: "Lorem ipsum donor amet siti ceali placeholder text",
+            url: "https://timerse.com"
+          },
+          {
+            imageSrc: celebrationIconImageSrc,
+            title: "Birthday Catering",
+            description: "Lorem ipsum donor amet siti ceali placeholder text",
+            url: "https://reddit.com"
+          }
+        ]}
 
-      <MobileNavLinksContainer css={collapseBreakpointCss.mobileNavLinksContainer}>
-        {logoLink}
-        <MobileNavLinks initial={{ x: "150%", display: "none" }} animate={animation} css={collapseBreakpointCss.mobileNavLinks}>
-          {links}
-        </MobileNavLinks>
-        <NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>
-          {showNavLinks ? <CloseIcon tw="w-6 h-6" /> : <MenuIcon tw="w-6 h-6" />}
-        </NavToggle>
-      </MobileNavLinksContainer>
-    </Header>
+        imageContainerCss={tw`p-2!`}
+        imageCss={tw`w-20! h-20!`}
+      />
+      <MainFeature2
+        subheading={<Subheading>A Reputed Brand</Subheading>}
+        heading={<>Why <HighlightedText>Choose Us ?</HighlightedText></>}
+        statistics={[
+          {
+            key: "Orders",
+            value: "94000+",
+          },
+          {
+            key: "Customers",
+            value: "11000+"
+          },
+          {
+            key: "Chefs",
+            value: "1500+"
+          }
+        ]}
+        primaryButtonText="Order Now"
+        primaryButtonUrl="https://order.now.com"
+        imageInsideDiv={false}
+        imageSrc="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEzNzI2fQ&auto=format&fit=crop&w=768&q=80"
+        imageCss={Object.assign(tw`bg-cover`, imageCss)}
+        imageContainerCss={tw`md:w-1/2 h-auto`}
+        imageDecoratorBlob={true}
+        imageDecoratorBlobCss={tw`left-1/2 md:w-32 md:h-32 -translate-x-1/2 opacity-25`}
+        textOnLeft={true}
+      />
+      <Testimonial
+        subheading=""
+        heading={<>Customers <HighlightedText>Love Us.</HighlightedText></>}
+      />
+      <DownloadApp
+        text={<>People around you are ordering delicious meals using the <HighlightedTextInverse>Treact App.</HighlightedTextInverse></>}
+      />
+      <Footer />
+    </AnimationRevealPage>
   );
-};
+}
 
-/* The below code is for generating dynamic break points for navbar.
- * Using this you can specify if you want to switch
- * to the toggleable mobile navbar at "sm", "md" or "lg" or "xl" above using the collapseBreakpointClass prop
- * Its written like this because we are using macros and we can not insert dynamic variables in macros
- */
+// export default EventLandingPage;
+// export default HotelTravelLandingPage;
+// export default AgencyLandingPage;
+// export default SaaSProductLandingPage;
+// export default RestaurantLandingPage;
+// export default ServiceLandingPage;
+// export default HostingCloudLandingPage;
 
-const collapseBreakPointCssMap = {
-  sm: {
-    mobileNavLinks: tw`sm:hidden`,
-    desktopNavLinks: tw`sm:flex`,
-    mobileNavLinksContainer: tw`sm:hidden`
-  },
-  md: {
-    mobileNavLinks: tw`md:hidden`,
-    desktopNavLinks: tw`md:flex`,
-    mobileNavLinksContainer: tw`md:hidden`
-  },
-  lg: {
-    mobileNavLinks: tw`lg:hidden`,
-    desktopNavLinks: tw`lg:flex`,
-    mobileNavLinksContainer: tw`lg:hidden`
-  },
-  xl: {
-    mobileNavLinks: tw`lg:hidden`,
-    desktopNavLinks: tw`lg:flex`,
-    mobileNavLinksContainer: tw`lg:hidden`
-  }
-};
+// export default LoginPage;
+// export default SignupPage;
+// export default PricingPage;
+// export default AboutUsPage;
+// export default ContactUsPage;
+// export default BlogIndexPage;
+// export default TermsOfServicePage;
+// export default PrivacyPolicyPage;
+
+// export default MainLandingPage;
